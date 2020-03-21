cf36f07218832e4712e19a4c5589d7b6bbf33444
Marc <mleese200@caledonian.ac.uk>
2020-03-21 17:29:06 +0000

    rss feed done

---
+++ app/src/main/java/com/example/mobilecoursework/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.mobilecoursework;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ArrayList<String> rssLinks = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnRoadworks = findViewById(R.id.roadworks);
+        Button btnPlanned = findViewById(R.id.planned);
+        Button btnCurrent = findViewById(R.id.current);
+
+        btnRoadworks.setOnClickListener(this);
+        btnPlanned.setOnClickListener(this);
+        btnCurrent.setOnClickListener(this);
+
+        rssLinks.add("https://trafficscotland.org/rss/feeds/plannedroadworks.aspx");
+        rssLinks.add("https://trafficscotland.org/rss/feeds/roadworks.aspx");
+        rssLinks.add("https://trafficscotland.org/rss/feeds/currentincidents.aspx");
+    }
+
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(getApplicationContext(), RSSFeedActivity.class);
+            switch (view.getId()) {
+                case R.id.roadworks:
+                    intent.putExtra("rssLink", rssLinks.get(1));
+                    startActivity(intent);
+                    break;
+                case R.id.planned:
+                    intent.putExtra("rssLink", rssLinks.get(0));
+                    startActivity(intent);
+                    break;
+                case R.id.current:
+                    intent.putExtra("rssLink", rssLinks.get(2));
+                    startActivity(intent);
+                    break;
+            }
+        }
+    }
---
+++ app/src/main/java/com/example/mobilecoursework/RssItems.java
@@ -0,0 +1,19 @@
+package com.example.mobilecoursework;
+
+public class RssItems {
+
+    public String title;
+    public String link;
+    public String description;
+    public String pubdate;
+    public String guid;
+
+    public RssItems(String title, String link, String description, String pubdate, String guid) {
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.pubdate = pubdate;
+        this.guid = guid;
+
+    }
+}
